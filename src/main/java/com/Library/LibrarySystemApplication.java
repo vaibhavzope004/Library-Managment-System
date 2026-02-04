c7d5df285249134e1792deeaf2afc6bd3ed53d61
vaibhavzope004 <vaibhavzope2004@gmail.com>
2026-02-04 14:25:34 +0530

    Update LibrarySystemApplication.java

--- src/main/java/com/Library/LibrarySystemApplication.java
+++ src/main/java/com/Library/LibrarySystemApplication.java
@@ -2,11 +2,14 @@ package com.Library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.Library.model")
 @EnableJpaRepositories(basePackages = "com.Library.repository")
 public class LibrarySystemApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(LibrarySystemApplication.class, args);
     }
